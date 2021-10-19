0eea42efd22b2513e375a49eefff11ea15b280f4
Karine CHAMPEIL <kchampeilpro@gmail.com>
2021-10-19 16:18:01 +0200

    initial commit

---
+++ patient/src/test/java/com/mesdiscreen/patient/PatientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mesdiscreen.patient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
