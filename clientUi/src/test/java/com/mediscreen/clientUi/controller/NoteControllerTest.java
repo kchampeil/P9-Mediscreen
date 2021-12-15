90f69fd7ad7c5bed78897ca2f155255e4388b19a
Karine CHAMPEIL <karine.champeil@axa.fr>
2021-12-15 16:23:40 +0100

    improving code coverage

--- clientUi/src/test/java/com/mediscreen/clientUi/controller/NoteControllerTest.java
+++ clientUi/src/test/java/com/mediscreen/clientUi/controller/NoteControllerTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
@@ -16,8 +17,10 @@ import com.mediscreen.clientUi.constants.TestConstants;
 import com.mediscreen.clientUi.constants.ViewNameConstants;
 import com.mediscreen.clientUi.proxies.INoteProxy;
 import com.mediscreen.clientUi.proxies.IPatientProxy;
+import com.mediscreen.commons.constants.ExceptionConstants;
 import com.mediscreen.commons.dto.NoteDTO;
 import com.mediscreen.commons.dto.PatientDTO;
+import com.mediscreen.commons.exceptions.PatientDoesNotExistException;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -84,11 +87,60 @@ public class NoteControllerTest {
                .andExpect(model().attributeExists("sortField"))
                .andExpect(model().attributeExists("sortDir"))
                .andExpect(model().attributeExists("reverseSortDir"))
+               .andExpect(model().attributeDoesNotExist("infoMessage"))
                .andExpect(view().name(ViewNameConstants.SHOW_ALL_NOTES_FOR_PATIENT));
 
         verify(patientProxyMock, Mockito.times(1))
             .getPatientById(anyInt());
         verify(noteProxyMock, Mockito.times(1))
+            .getAllNotesForPatientByPage(anyInt(), anyInt(), anyInt(), any(String.class), any(String.class));
+    }
+
+    @Test
+    void showAllNotesForPatientByPage_WithNoNotesForPatient_returnsInfoMessage() throws Exception {
+
+        List<NoteDTO> noteDTOList = new ArrayList<>();
+        Page<NoteDTO> noteDTOPage = new PageImpl<>(noteDTOList);
+
+        when(patientProxyMock.getPatientById(anyInt())).thenReturn(patientDTO);
+        when(noteProxyMock.getAllNotesForPatientByPage(anyInt(), anyInt(), anyInt(), any(String.class),
+                                                       any(String.class)))
+            .thenReturn(noteDTOPage);
+
+        mockMvc.perform(get("/note/{patientId}/list/{page}", 1, 1)
+                            .param("patientId", String.valueOf(noteDTO.getPatientId()))
+                            .param("page", "1")
+                            .param("sortField", "id")
+                            .param("sortDir", "asc")
+                            .param("itemsPerPage", "10"))
+               .andExpect(status().isOk())
+               .andExpect(model().attributeExists("infoMessage"))
+               .andExpect(view().name(ViewNameConstants.SHOW_ALL_NOTES_FOR_PATIENT));
+
+        verify(patientProxyMock, Mockito.times(1))
+            .getPatientById(anyInt());
+        verify(noteProxyMock, Mockito.times(1))
+            .getAllNotesForPatientByPage(anyInt(), anyInt(), anyInt(), any(String.class), any(String.class));
+    }
+
+    @Test
+    void showAllNotesForPatientByPage_forUnknownPatient_returnsPatientListView() throws Exception {
+
+        when(patientProxyMock.getPatientById(anyInt())).thenThrow(new PatientDoesNotExistException(
+            ExceptionConstants.PATIENT_NOT_FOUND + TestConstants.UNKNOWN_PATIENT_ID));
+
+        mockMvc.perform(get("/note/{patientId}/list/{page}", TestConstants.UNKNOWN_PATIENT_ID, 1)
+                            .param("patientId", String.valueOf(TestConstants.UNKNOWN_PATIENT_ID))
+                            .param("page", "1")
+                            .param("sortField", "id")
+                            .param("sortDir", "asc")
+                            .param("itemsPerPage", "10"))
+               .andExpect(status().isFound())
+               .andExpect(redirectedUrl(ViewNameConstants.SHOW_ALL_PATIENTS));
+
+        verify(patientProxyMock, Mockito.times(1))
+            .getPatientById(anyInt());
+        verify(noteProxyMock, Mockito.times(0))
             .getAllNotesForPatientByPage(anyInt(), anyInt(), anyInt(), any(String.class), any(String.class));
     }
 }
