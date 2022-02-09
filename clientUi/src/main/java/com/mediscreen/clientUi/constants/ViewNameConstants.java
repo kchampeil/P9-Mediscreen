4631705c7f5d632cb12587391f3161f637261d7c
Karine CHAMPEIL <kchampeil.pro@gmail.com>
2022-02-09 11:05:07 +0100

    docker-compose implemented (except mongodb to do) slash removed for view names

--- clientUi/src/main/java/com/mediscreen/clientUi/constants/ViewNameConstants.java
+++ clientUi/src/main/java/com/mediscreen/clientUi/constants/ViewNameConstants.java
@@ -7,15 +7,15 @@ public class ViewNameConstants {
     /* Patient Views */
     public static final String HOME_ORGANIZER = "/organizer";
     public static final String HOME_DOCTOR = "/doctor";
-    public static final String SHOW_ALL_PATIENTS = "/patient/list";
-    public static final String UPDATE_PATIENT = "/patient/update";
-    public static final String ADD_PATIENT = "/patient/add";
+    public static final String SHOW_ALL_PATIENTS = "patient/list";
+    public static final String UPDATE_PATIENT = "patient/update";
+    public static final String ADD_PATIENT = "patient/add";
 
     /* Note Views */
-    public static final String SHOW_ALL_NOTES_FOR_PATIENT = "/note/list";
-    public static final String ADD_NOTE = "/note/add";
-    public static final String UPDATE_NOTE = "/note/update";
+    public static final String SHOW_ALL_NOTES_FOR_PATIENT = "note/list";
+    public static final String ADD_NOTE = "note/add";
+    public static final String UPDATE_NOTE = "note/update";
 
     /* Risk Assessment Views */
-    public static final String RISK_ASSESSMENT_RESULT = "/riskAssessment/assess";
+    public static final String RISK_ASSESSMENT_RESULT = "riskAssessment/assess";
 }
